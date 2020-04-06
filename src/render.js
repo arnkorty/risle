3a6ae94fe41b90a33924f11f5a9f51a30b38dcfa
fuanzhao <arnkorty.fu@gmail.com>
2020-04-06 14:08:04 +0800

    tab convert space

--- src/render.js
+++ src/render.js
@@ -4,24 +4,24 @@ import { createElement, Fragment } from './element';
 // import options from './options';
 
 export function render(vnode, parentDom, replaceNode) {
-	// if (options._root) options._root(vnode, parentDom);
+  // if (options._root) options._root(vnode, parentDom);
 
-	// let isHydrating = replaceNode === IS_HYDRATE;
-	let oldVNode =  replaceNode && replaceNode._children || parentDom._children;
-	vnode = createElement(Fragment, null, [vnode]);
+  // let isHydrating = replaceNode === IS_HYDRATE;
+  let oldVNode =  replaceNode && replaceNode._children || parentDom._children;
+  vnode = createElement(Fragment, null, [vnode]);
 
-	let mounts = [];
-	diff(
-		parentDom,
-		(replaceNode || parentDom)._children = vnode,
-		oldVNode || EMPTY_OBJ,
-		replaceNode
-			? [replaceNode]
-			: oldVNode
-				? null
-				: EMPTY_ARR.slice.call(parentDom.childNodes),
-		mounts,
-		replaceNode || EMPTY_OBJ,
-	);
-	commitRoot(mounts, vnode);
+  let mounts = [];
+  diff(
+    parentDom,
+    (replaceNode || parentDom)._children = vnode,
+    oldVNode || EMPTY_OBJ,
+    replaceNode
+      ? [replaceNode]
+      : oldVNode
+        ? null
+        : EMPTY_ARR.slice.call(parentDom.childNodes),
+    mounts,
+    replaceNode || EMPTY_OBJ,
+  );
+  commitRoot(mounts, vnode);
 }
--- src/util.js
+++ src/util.js
@@ -1,21 +1,31 @@
 export function assign(obj, ...args) {
-	return Object.assign(obj, ...args)
+  return Object.assign(obj, ...args)
 }
 
 export function removeNode(node) {
-	let parentNode = node.parentNode;
-	if (parentNode) parentNode.removeChild(node);
+  let parentNode = node.parentNode;
+  if (parentNode) parentNode.removeChild(node);
 }
 
 export function isFn(fn) {
-	return typeof fn === 'function'
+  return typeof fn === 'function'
 }
 
 export function isChanged(a, b) {
-	return !a || b.some((item, index) => item !== a[index])
+  return !a || b.some((item, index) => item !== a[index])
 }
 
 export const defer = (fn, cb) => {
+<<<<<<< HEAD
 	return Promise.resolve().then(fn).then(value =>  cb ? cb(value) : value)
+=======
+  return requestAnimationFrame(() =>{
+    const value = fn()
+    if (cb) {
+      return cb(value)
+    }
+    return value
+  })
+>>>>>>> ff5b0b3... tab convert space
 }
 // typeof Promise=='function' ? Promise.prototype.then.bind(Promise.resolve()) : setTimeout;
